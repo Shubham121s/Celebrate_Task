61dc86ea4036edc2f24a3c3382bff712819f9aa2
Shubham Kumar Poddar <113991071+Shubham121s@users.noreply.github.com>
2024-09-09 16:56:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function show() {
+    const img = document.getElementById('img');
+    const btn = document.getElementById('btn');
+    const container1 = document.getElementsByClassName('container1')
+    img.classList.add('animate-img');
+    btn.style.display = 'none'
+    container1.style.display="none"
+
+}
